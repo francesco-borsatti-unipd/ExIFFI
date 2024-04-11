7f666df5468eb2bc259e7deea53ae8bef47528e3
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-04-11 11:49:15 +0200

    Update sh file to launch experiments

--- experiments/launch_local_imp_exp.sh
+++ experiments/launch_local_imp_exp.sh
@@ -2,12 +2,12 @@
 
 SCRIPT_PATH="test_local_importances.py"
 
-DATASETS="piade_s2"
+DATASETS="piade_s2_all_alarms"
 
 DATASET_PATH="../../datasets/data/PIADE/"
 
 python $SCRIPT_PATH \
         --dataset_name $DATASETS \
         --dataset_path $DATASET_PATH \
-        --model "EIF" \
-        --interpretation "EXIFFI" 
+        --model "EIF+" \
+        --interpretation "EXIFFI+" 
