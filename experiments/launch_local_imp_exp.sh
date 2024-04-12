b909eb3b89c14b13dadf01bea49c58a5450e50a0
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-04-12 07:25:10 +0200

    Experiments and remove some files

--- experiments/launch_local_imp_exp.sh
+++ experiments/launch_local_imp_exp.sh
@@ -9,7 +9,7 @@ DATASET_PATH="../../datasets/data/PIADE/"
 python $SCRIPT_PATH \
         --dataset_name $DATASETS \
         --dataset_path $DATASET_PATH \
-        --model "EIF" \
-        --interpretation "EXIFFI" \
+        --model "EIF+" \
+        --interpretation "EXIFFI+" \
         --pre_process
 
