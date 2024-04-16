67654d9e19da5dd44c747b7b596f32b55f8abf3f
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-04-16 19:22:29 +0200

    experiments

--- experiments/launch_local_imp_exp.sh
+++ experiments/launch_local_imp_exp.sh
@@ -7,10 +7,14 @@ DATASETS="piade_s2"
 DATASET_PATH="../../datasets/data/PIADE/"
 
 python $SCRIPT_PATH \
-        --pre_process \
-        --contamination 0.05 \
         --n_estimators 300 \
+        --contamination 0.01 \
         --model "EIF+" \
         --dataset_path $DATASET_PATH \
         --dataset_name $DATASETS \
-        --interpretation "EXIFFI+" 
+        --interpretation "EXIFFI+" \
+        --scenario 1 \
+        --get_labels 
+
+# To pre_process the data, add the following line:
+# --pre_process \
