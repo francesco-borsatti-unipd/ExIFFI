96231f4ebebbc616e2ad695907a2bda09f9bde4b
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-04-17 09:48:04 +0200

    Experiment results

--- experiments/launch_local_imp_exp.sh
+++ experiments/launch_local_imp_exp.sh
@@ -9,10 +9,10 @@ DATASET_PATH="../../datasets/data/PIADE/"
 python $SCRIPT_PATH \
         --n_estimators 300 \
         --contamination 0.01 \
-        --model "EIF+" \
+        --model "EIF" \
         --dataset_path $DATASET_PATH \
         --dataset_name $DATASETS \
-        --interpretation "EXIFFI+" \
+        --interpretation "EXIFFI" \
         --scenario 1 \
         --get_labels 
 
