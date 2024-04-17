18943f30b40c5b2da0df0b69e3ccf67adf697cbc
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-04-17 17:47:30 +0200

    Experiments

--- experiments/launch_local_imp_exp.sh
+++ experiments/launch_local_imp_exp.sh
@@ -7,14 +7,19 @@ DATASETS="piade_s2"
 DATASET_PATH="../../datasets/data/PIADE/"
 
 python $SCRIPT_PATH \
-        --n_estimators 300 \
+        --n_estimators 100 \
         --contamination 0.01 \
-        --model "EIF" \
+        --model "EIF+" \
         --dataset_path $DATASET_PATH \
         --dataset_name $DATASETS \
-        --interpretation "EXIFFI" \
+        --interpretation "EXIFFI+" \
         --scenario 1 \
-        --get_labels 
+        --n_runs 40 \
+        --pre_process
 
 # To pre_process the data, add the following line:
 # --pre_process \
+
+# To get the labels, add the following line. With the new configuration this makes sense
+# only if n_runs=1
+# --get_labels \
