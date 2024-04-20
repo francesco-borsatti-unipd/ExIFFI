2b7bcd7f9bb1a551162adcd0112a684a052b8c21
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-04-20 17:51:47 +0200

    Experiment results

--- experiments/launch_get_metrics.sh
+++ experiments/launch_get_metrics.sh
@@ -2,50 +2,15 @@
 
 SCRIPT_PATH="get_metrics.py"
 
-DATASETS="wine"
+DATASETS="piade_s2"
 
-DATASET_PATH="../data/real/"
-
-# MODELS="EIF+ EIF IF DIF AnomalyAutoencoder"
-# IFS=' ' read -ra MODELS_ARRAY <<< "$MODELS"
-# SCENARIOS=(2 1)
-
-# for model in "${MODELS_ARRAY[@]}"; do
-#     for scenario in "${SCENARIOS[@]}"; do
-
-#        python $SCRIPT_PATH \
-#             --dataset_name $DATASETS \
-#             --dataset_path $DATASET_PATH \
-#             --model "$model" \
-#             --scenario "$scenario"
-
-#     done
-# done
+DATASET_PATH="../../datasets/data/PIADE/"
 
 python $SCRIPT_PATH \
             --dataset_name $DATASETS \
             --dataset_path $DATASET_PATH \
-            --model "DIF" \
+            --model "EIF+" \
+            --interpretation "EXIFFI+" \
+            --n_estimators 300 \
             --pre_process \
             --scenario 2
-
-python $SCRIPT_PATH \
-            --dataset_name $DATASETS \
-            --dataset_path $DATASET_PATH \
-            --model "DIF" \
-            --pre_process \
-            --scenario 1
-
-python $SCRIPT_PATH \
-            --dataset_name $DATASETS \
-            --dataset_path $DATASET_PATH \
-            --model "AnomalyAutoencoder" \
-            --pre_process \
-            --scenario 2
-
-python $SCRIPT_PATH \
-            --dataset_name $DATASETS \
-            --dataset_path $DATASET_PATH \
-            --model "AnomalyAutoencoder" \
-            --pre_process \
-            --scenario 1
--- experiments/launch_metrics_exp.sh
+++ experiments/launch_metrics_exp.sh
@@ -1,106 +1,33 @@
 #!/bin/bash
 
-# Path to the Python script to execute
-#SCRIPT_PATH="test_global_importancies.py"
-#SCRIPT_PATH="test_feature_selection.py"
 SCRIPT_PATH="test_metrics.py"
-#SCRIPT_PATH="test_GFI_FS.py"
-#SCRIPT_PATH="test_local_importances.py"
-#SCRIPT_PATH="test_time_scaling.py"
 
 # List of datasets
-#DATASETS="Xaxis Yaxis bisect bisect_3d bisect_6d"
-#DATASETS="glass cardio pima breastw ionosphere annthyroid pendigits diabetes shuttle moodify"
-#DATASETS="Yaxis bisect bisect_3d bisect_6d"
-DATASETS="annthyroid"
-
-# Split the DATASETS string into an array
-IFS=' ' read -ra DATASET_ARRAY <<< "$DATASETS"
+DATASETS="piade_s2"
 
 # Path to the datasets 
-DATASET_PATH="../data/real/"
+DATASET_PATH="../../datasets/data/PIADE/"
 
-# Iterate over the datasets and call the Python command for each dataset
-for dataset in "${DATASET_ARRAY[@]}"; do
- 
-    # python $SCRIPT_PATH \
-    #     --dataset_name "$dataset" \
-    #     --dataset_path "$DATASET_PATH" \
-    #     --model "EIF+" \
-    #     --scenario 2
+# For TEP 
 
-    # python $SCRIPT_PATH \
-    #     --dataset_name "$dataset" \
-    #     --dataset_path "$DATASET_PATH" \
-    #     --model "EIF+" \
-    #     --scenario 1
+# python $SCRIPT_PATH \
+#     --dataset_name $DATASETS \
+#     --dataset_path $DATASET_PATH \
+#     --model "EIF+" \
+#     --interpretation "EXIFFI+" \
+#     --n_estimators 300 \
+#     --scenario 2 \
+#     --pre_process 1 \
+#     --compute_GFI 1 \
+#     --compute_perf
 
+# For PIADE
 
-    # python $SCRIPT_PATH \
-    #         --dataset_name "$dataset" \
-    #         --dataset_path "$DATASET_PATH" \
-    #         --model "EIF" \
-    #         --scenario 2
-
-    # python $SCRIPT_PATH \
-    #     --dataset_name "$dataset" \
-    #     --dataset_path "$DATASET_PATH" \
-    #     --model "EIF" \
-    #     --scenario 1
-
-    # python $SCRIPT_PATH \
-    #     --dataset_name "$dataset" \
-    #     --dataset_path "$DATASET_PATH" \
-    #     --model "IF" \
-    #     --scenario 2
-
-    # python $SCRIPT_PATH \
-    #     --dataset_name "$dataset" \
-    #     --dataset_path "$DATASET_PATH" \
-    #     --model "IF" \
-    #     --scenario 1
-
-    python $SCRIPT_PATH \
-        --dataset_name "$dataset" \
-        --dataset_path "$DATASET_PATH" \
-        --model "DIF" \
-        --scenario 2
-
-    python $SCRIPT_PATH \
-        --dataset_name "$dataset" \
-        --dataset_path "$DATASET_PATH" \
-        --model "DIF" \
-        --scenario 1
-
-    python $SCRIPT_PATH \
-        --dataset_name "$dataset" \
-        --dataset_path "$DATASET_PATH" \
-        --model "AnomalyAutoencoder" \
-        --scenario 2
-
-    python $SCRIPT_PATH \
-        --dataset_name "$dataset" \
-        --dataset_path "$DATASET_PATH" \
-        --model "AnomalyAutoencoder" \
-        --scenario 1
-done
-
-# --interpretation "DIFFI" \
-# --compute_GFI 1
-
-# For local scoremaps experiments 
-#--feats_plot "(10,0)" \
-#
-
-# To use the pre-processing step
-#--pre_process \
-
-# For Feature Selection experiments
-# --model_interpretation "IF" \
-# --interpretation "RandomForest" \
-
-# For Performance Metrics experiments
-
-# --interpretation "EXIFFI+" \
-#         --feats_plot "(1,5)" \
-#         --pre_process \
+python $SCRIPT_PATH \
+    --dataset_name $DATASETS \
+    --dataset_path $DATASET_PATH \
+    --model "EIF+" \
+    --interpretation "EXIFFI+" \
+    --n_estimators 300 \
+    --scenario 2 \
+    --compute_GFI 1
