fix centroid importances normalization

model_reboot/EIF_reboot.py
@@ -107,6 +107,7 @@ class ExtendedTree:
         plus: bool = True,
         max_nodes: int = 10000,
         eta: float = 1.5,
+        use_centroid_importance: bool = False,
     ):
 
         if locked_dims >= d:
@@ -121,6 +122,7 @@ class ExtendedTree:
         self.max_nodes = max_nodes
         self.eta = eta
         self.num_nodes = 0
+        self.use_centroid_importance = use_centroid_importance
 
     def fit(self, X: np.ndarray) -> None:
         """
@@ -225,6 +227,9 @@ class ExtendedTree:
                 cumul_normals,
                 cumul_importance,
                 mask,
+                self.use_centroid_importance,
+                X,
+                subset_ids,
             )
 
             alloc_child_nodes(self.nodes, self.max_nodes, self.num_nodes, node)
@@ -343,6 +348,7 @@ class ExtendedIsolationForest:
         max_depth: Union[str, int] = "auto",
         max_samples: Union[str, int] = "auto",
         eta: float = 1.5,
+        use_centroid_importance: bool = False,
     ):
         self.n_estimators = n_estimators
         self.max_samples = 256 if max_samples == "auto" else max_samples
@@ -352,6 +358,7 @@ class ExtendedIsolationForest:
         self.ids = None
         self.X = None
         self.eta = eta
+        self.use_centroid_importance = use_centroid_importance
 
     @property
     def avg_number_of_nodes(self):
@@ -384,6 +391,7 @@ class ExtendedIsolationForest:
                 locked_dims=locked_dims,
                 plus=self.plus,
                 eta=self.eta,
+                use_centroid_importance=self.use_centroid_importance,
             )
             for _ in range(self.n_estimators)
         ]
@@ -457,12 +465,13 @@ class ExtendedIsolationForest:
         importances = np.zeros(X.shape)
         normals = np.zeros(X.shape)
         for i, tree in enumerate(self.trees):
-            # importance, normal = tree.importances(ids[i])
-            # importances += importance
-            # normals += normal
+            
+            importance, normal = tree.importances(ids[i])
+            importances += importance
+            normals += normal
 
-            importances += tree.cumul_importances[ids[i]]
-            normals += tree.cumul_normals[ids[i]]
+            # importances += tree.cumul_importances[ids[i]]
+            # normals += tree.cumul_normals[ids[i]]
 
         return importances / self.n_estimators, normals / self.n_estimators
 

model_reboot/c_functions/c_signatures.py
@@ -302,6 +302,25 @@ c_update_importances_and_normals.argtypes = [
 ]
 c_update_importances_and_normals.restype = None
 
+c_update_importances_and_normals_centroid = lib.update_importances_and_normals_centroid
+c_update_importances_and_normals_centroid.argtypes = [
+    c.POINTER(Node),
+    c.c_int,
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C_CONTIGUOUS"),
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C_CONTIGUOUS"),
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C_CONTIGUOUS"),
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C_CONTIGUOUS"),
+    np.ctypeslib.ndpointer(dtype=np.bool_, ndim=1, flags="C_CONTIGUOUS"),
+    c.c_int,
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C_CONTIGUOUS"), # flattened X dataset
+    c.c_int, # n_samples in the dataset
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C_CONTIGUOUS"), # flattened left subset samples
+    c.c_int, # n_samples in the left subset
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C_CONTIGUOUS"), # flattened right subset samples
+    c.c_int # n_samples in the right subset
+]
+c_update_importances_and_normals_centroid.restype = None
+
 
 def update_importances_and_normals(
     node,
@@ -309,6 +328,9 @@ def update_importances_and_normals(
     cumul_normal,
     cumul_importance,
     mask,
+    use_centroid_importance,
+    dataset,
+    subset_ids,
 ):
     """
     Compute the cumulative sum of the importances and the sum of the normal vectors of the
@@ -326,14 +348,33 @@ def update_importances_and_normals(
     """
     l_cumul_importance = np.zeros(d)
     r_cumul_importance = np.zeros(d)
-    c_update_importances_and_normals(
-        node,
-        d,
-        cumul_normal,
-        cumul_importance,
-        l_cumul_importance,
-        r_cumul_importance,
-        mask,
-        mask.shape[0],
-    )
+
+    if use_centroid_importance:
+        c_update_importances_and_normals_centroid(
+            node,
+            d,
+            cumul_normal,
+            cumul_importance,
+            l_cumul_importance,
+            r_cumul_importance,
+            mask,
+            mask.shape[0],
+            dataset.flatten(),
+            dataset.shape[0],
+            dataset[subset_ids[mask]].flatten().astype(np.float64),
+            len(subset_ids[mask]),
+            dataset[subset_ids[~mask]].flatten().astype(np.float64),
+            len(subset_ids[~mask]),
+        )
+    else:
+        c_update_importances_and_normals(
+            node,
+            d,
+            cumul_normal,
+            cumul_importance,
+            l_cumul_importance,
+            r_cumul_importance,
+            mask,
+            mask.shape[0],
+        )
     return l_cumul_importance, r_cumul_importance